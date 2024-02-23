348884d8b1c44d2bdd27a603476cf322dc2f1cd7
Anthony Boutherin <anthony.boutherin@gmail.com>
2024-02-23 10:17:13 +0100

    buttonNav

--- script/nav.js
+++ script/nav.js
@@ -1,11 +1,16 @@
-const button = document.querySelector(".buttonNav");
+const button = document.querySelector("button");
 console.log(button);
 
 button.addEventListener("click", buttonNav);
 
 function buttonNav() {
   console.log("click !");
-  if (button.classList === "buttonNav") {
+  if (button.classList.contains("rotate")) {
+    console.log("On enlève la classe : rotate");
+    button.classList.remove("rotate");
+  } else {
+    button.classList.add("rotate");
+    // button.textContent = "X";
     console.log("On ajoute la classe : rotate");
   }
 }
