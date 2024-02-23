1e7377e1c2cc25390c8cfc716ff29522ea0352f5
Anthony Boutherin <anthony.boutherin@gmail.com>
2024-02-23 09:48:26 +0100

    test

--- script/nav.js
+++ script/nav.js
@@ -1,19 +1,11 @@
-// On veut que lorsqu'on clique sur le bouton menu, 
-//il tourne de 90degré et déplie un menu navigateur
-// s'il est ouvert le = devient X
-
 const button = document.querySelector(".buttonNav");
 console.log(button);
 
-button.addEventListener('click', buttonNav);
+button.addEventListener("click", buttonNav);
 
 function buttonNav() {
-    console.log("click !");
-    if (button.classList = "") {
-        button.classList.add("rotate");
-        console.log("On ajoute la classe : rotate");
-    } else {
-        button.classList.remove("rotate");
-        console.log("On retire la classe : rotate");
-    }
+  console.log("click !");
+  if (button.classList === "buttonNav") {
+    console.log("On ajoute la classe : rotate");
+  }
 }
